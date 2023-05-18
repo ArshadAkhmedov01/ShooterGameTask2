9764a3fa4d845bc96de2b2c3775b0eb972fb531e
DESKTOP-TOUK27O\Dell <ersadehmedov01@gmail.com>
2023-05-18 14:59:44 +0400

    Bug fixed

--- UniGameTask/Assets/Scenes/New Folder/Scripts/Oyuncu.cs
+++ UniGameTask/Assets/Scenes/New Folder/Scripts/Oyuncu.cs
@@ -13,9 +13,16 @@ public class Oyuncu : MonoBehaviour
     [SerializeField] float speed = 6f;
 
     [SerializeField] int Puanlar;
+
+
+    Vector3 Direction;
+    float CurrentTurnAngle;
+    float SmoothTurnTime = 0.04f;
+    float Angle;
+    Rigidbody rb;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -32,13 +39,27 @@ public class Oyuncu : MonoBehaviour
         {
             transform.rotation *= Quaternion.Euler(0f, 1f, 0f * speed* Time.deltaTime);
         }
-        
-
-        vertical = Input.GetAxis("Vertical");
         horizontal = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(horizontal * Time.deltaTime, 0f ,vertical * Time.deltaTime) * speed;
+        vertical = Input.GetAxis("Vertical");
 
-                                                   
+        Direction = new Vector3(horizontal, 0f, vertical);
+        if (Direction.magnitude > 0.01f)
+        {
+            float TargetAngle = Mathf.Atan2(Direction.x, Direction.z) * Mathf.Rad2Deg;
+            Angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, TargetAngle, ref CurrentTurnAngle, SmoothTurnTime);
+            transform.rotation = Quaternion.Euler(0f, Angle, 0f);
+            rb.MovePosition(transform.position + (Direction.normalized * speed * Time.deltaTime));
+        }
+
+
+
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -12f, 14f), transform.position.y, Mathf.Clamp(transform.position.z, -10f, 10f));
+        //float PosX = Mathf.Clamp(transform.position.x,-10f, 10f);
+        //transform.position = new Vector3(Mathf.Clamp(transform.position.x, -12f, 14f), transform.position.z, -12f, 19f);
+        //transform.Translate(Mathf.Clamp(transform.position.x, -12f, 14f), transform.position.z, 19f);
+
+
 
     }
 
